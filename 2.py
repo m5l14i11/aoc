6ffa4b61bc7c4428e8e3a32f8257cf94fa6efb8d
Siarhei Melnik <siarhei.m.a@gmail.com>
2021-01-21 21:54:03 +0300

    2

---
+++ 2.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+with open('2.txt', encoding = 'utf-8') as f:
+    lines = f.readlines()
+    c = Counter()
+
+    ans1 = 0
+    ans2 = 0
+
+    for line in lines:
+        [n, symbol, password] = line.strip().split(' ')
+        
+        n_range = n.split('-')
+        symbol = symbol.replace(':', '')
+
+        lower = int(n_range[0])
+        upper = int(n_range[1])
+
+        c = Counter(password)
+        count = c[symbol]
+
+        if count >= lower and count <= upper:
+            ans1 += 1
+
+        first = password[lower - 1] == symbol
+        second = password[upper - 1] == symbol
+
+        if (first and not second) or (second and not first):
+            ans2 += 1
+        
+    print(ans1)
+    print(ans2)
+        
