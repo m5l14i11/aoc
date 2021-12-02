a668b66bdb2dbda6db8457a39bc0f408c6c61353
Siarhei Melnik <siarhei.m.a@gmail.com>
2021-12-02 13:21:58 +0300

    2021-02

---
+++ 2021/2.py
@@ -0,0 +1,42 @@
+def first_solution(data):
+    horiz = 0
+    depth = 0
+    for (move, pos) in data:
+        pos = int(pos)
+        if move == 'forward':
+            horiz += pos
+        if move == 'down':
+            depth += pos
+        if move == 'up':
+            depth -= pos
+
+    return horiz * depth
+
+def second_solution(data):
+    horiz = 0
+    depth = 0
+    aim = 0
+
+    for (move, pos) in data:
+        pos = int(pos)
+        if move == 'forward':
+            horiz += pos
+            depth += aim * pos
+        if move == 'down':
+            aim += pos
+        if move == 'up':
+            aim -= pos
+
+    return horiz * depth
+
+
+with open('2.txt') as f:
+    data = [x.strip() for x in f.readlines()]
+    data = [tuple(i.split(' ')) for i in data]
+    ans1 = first_solution(data)
+    print(ans1)
+    ans2 = second_solution(data)
+    print(ans2)
+    
+    
+
