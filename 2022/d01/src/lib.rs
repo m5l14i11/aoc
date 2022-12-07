b09019c7021824a68cde024c0380fc44c2d57fe2
Siarhei Melnik <siarhei.m.a@gmail.com>
2022-12-07 18:38:06 +0300

    format

--- 2022/d01/src/lib.rs
+++ 2022/d01/src/lib.rs
@@ -1,19 +1,23 @@
 fn get_elves(input: &str) -> Vec<usize> {
-    return input.split("\n\n").map(|calories| 
-        calories.lines().map(|item| item.parse::<usize>().unwrap()).sum::<usize>()).collect()
+    input
+        .split("\n\n")
+        .map(|calories| {
+            calories
+                .lines()
+                .map(|item| item.parse::<usize>().unwrap())
+                .sum::<usize>()
+        })
+        .collect()
 }
 
 pub fn solution_1(input: &str) -> usize {
-    return get_elves(input)
-        .into_iter()
-        .max()
-        .unwrap()
+    get_elves(input).into_iter().max().unwrap()
 }
 
 pub fn solution_2(input: &str) -> usize {
     let mut result = get_elves(input);
-    
+
     result.sort_by(|a, b| b.cmp(a));
 
-    return result.iter().take(3).sum()
+    result.iter().take(3).sum()
 }
